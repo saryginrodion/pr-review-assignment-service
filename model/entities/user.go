762bd35b510dbc3f2b581b347a5d6ed505490a61
Rodion <saryginrodion7@proton.me>
2025-11-22 00:08:33 +0700

    feat: updated user and team entities

--- model/entities/user.go
+++ model/entities/user.go
@@ -1,11 +1,9 @@
 package entities
 
-import "github.com/google/uuid"
-
 type User struct {
-	ID       uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
-	Username string    `gorm:"not null"`
-	TeamName string
-	Team     Team `gorm:"foreignKey:TeamName;references:TeamName"`
-	IsActive bool
+	ID       string `gorm:"primaryKey"`
+	Username string `gorm:"not null"`
+	TeamName string `gorm:"not null"`
+	Team     Team   `gorm:"foreignKey:TeamName;references:TeamName"`
+	IsActive bool   `gorm:"not null"`
 }
